581edb705147bcbd384524cfb824b1f171e11ff0
Inhyuk Na <inhyukna@Inhyukui-iMac-Pro.local>
2019-02-27 09:56:59 +0900

    trial 11 - reduced hidden num

--- train_all.sh
+++ train_all.sh
@@ -28,15 +28,15 @@ toy=""
 DATE=`date '+%Y-%m-%d-%H:%M:%S'`
 
 data_root=generated_datasets/generated_data${d_type}
-save_dir="${data_root}/saved_models_trial10"
+save_dir="${data_root}/saved_models_trial11"
 log_dir=${save_dir}/train_log
 mkdir -p ${save_dir}
 mkdir -p ${log_dir}
 
 
-export CUDA_VISIBLE_DEVICES=3
+export CUDA_VISIBLE_DEVICES=4
 echo "using gpu::" $CUDA_VISIBLE_DEVICES
-echo "trial10 - column co-attention layer added"
+echo "trial11 - reduced hidden"
 
 module=from
 epoch=600
