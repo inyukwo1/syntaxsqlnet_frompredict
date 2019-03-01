28449d40584724510d031ab4c834837cef16484b
Inhyuk Na <inhyukna@Inhyukui-iMac-Pro.local>
2019-03-01 12:13:23 +0900

    trial 19-half bert

--- train_all.sh
+++ train_all.sh
@@ -28,15 +28,15 @@ toy=""
 DATE=`date '+%Y-%m-%d-%H:%M:%S'`
 
 data_root=generated_datasets/generated_data${d_type}
-save_dir="${data_root}/saved_models_trial18"
+save_dir="${data_root}/saved_models_trial19"
 log_dir=${save_dir}/train_log
 mkdir -p ${save_dir}
 mkdir -p ${log_dir}
 
 
-export CUDA_VISIBLE_DEVICES=6
+export CUDA_VISIBLE_DEVICES=0
 echo "using gpu::" $CUDA_VISIBLE_DEVICES
-echo "trial 18 - use bert"
+echo "trial 19 - half bert"
 
 module=from
 epoch=600
