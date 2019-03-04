9520926b9cd542e96461df84ebd07fe0434ad855
Inhyuk Na <inhyukna@Inhyukui-iMac-Pro.local>
2019-03-04 10:34:00 +0900

    increased hidden dim

--- train_all.sh
+++ train_all.sh
@@ -28,15 +28,15 @@ toy=""
 DATE=`date '+%Y-%m-%d-%H:%M:%S'`
 
 data_root=generated_datasets/generated_data${d_type}
-save_dir="${data_root}/saved_models_trial24"
+save_dir="${data_root}/saved_models_trial25"
 log_dir=${save_dir}/train_log
 mkdir -p ${save_dir}
 mkdir -p ${log_dir}
 
 
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
 echo "using gpu::" $CUDA_VISIBLE_DEVICES
-echo "trial 24 - unused table encoded"
+echo "trial 25 - unused table encoded"
 
 module=from
 epoch=600
