6113f31b24889c9638b7d3b0d5476f31048c08c6
Inhyuk Na <ina@dblab.postech.ac.kr>
2019-03-15 17:31:09 +0900

    trial 7-34 embedding optimize too

--- train_all.sh
+++ train_all.sh
@@ -28,15 +28,15 @@ toy=""
 DATE=`date '+%Y-%m-%d-%H:%M:%S'`
 
 data_root=generated_datasets/generated_data${d_type}
-save_dir="${data_root}/saved_models_trial7-33"
+save_dir="${data_root}/saved_models_trial7-34"
 log_dir=${save_dir}/train_log
 mkdir -p ${save_dir}
 mkdir -p ${log_dir}
 
 
-export CUDA_VISIBLE_DEVICES=6
+export CUDA_VISIBLE_DEVICES=7
 echo "using gpu::" $CUDA_VISIBLE_DEVICES
-echo "trial 7-33 remove special tokens!"
+echo "trial 7-34 embedding learning too!"
 
 module=from
 epoch=600
