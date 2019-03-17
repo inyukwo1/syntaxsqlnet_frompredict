6f70836118254b3a4ec27268a7d1a42cc594fdfc
Inhyuk Na <ina@dblab.postech.ac.kr>
2019-03-18 08:57:42 +0900

    7-39 random seed changed

--- train.py
+++ train.py
@@ -56,7 +56,7 @@ if __name__ == '__main__':
         args.history_type = "full"
         use_hs = False
 
-    random_seed_set(2000)
+    random_seed_set(2001)
 
     N_word=300
     B_word=42
--- train_all.sh
+++ train_all.sh
@@ -28,15 +28,15 @@ toy=""
 DATE=`date '+%Y-%m-%d-%H:%M:%S'`
 
 data_root=generated_datasets/generated_data${d_type}
-save_dir="${data_root}/saved_models_trial7-38"
+save_dir="${data_root}/saved_models_trial7-39"
 log_dir=${save_dir}/train_log
 mkdir -p ${save_dir}
 mkdir -p ${log_dir}
 
 
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
 echo "using gpu::" $CUDA_VISIBLE_DEVICES
-echo "trial 7-38 5-vector!"
+echo "trial 7-39 random seed changed!"
 
 module=from
 epoch=600
