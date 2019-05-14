dc1026e28f4dbd44eed56b4fee5c0677862e5f5e
Inhyuk Na <ina@dblab.postech.ac.kr>
2019-05-14 15:29:37 +0900

    seq2seq models imported

--- models/from_predictor.py
+++ models/from_predictor.py
@@ -201,6 +201,45 @@ class WikiSQLCol(nn.Module):
         return score
 
 
+class AttnDecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size, dropout_p=0.1, max_length=500):
+        super(AttnDecoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.dropout_p = dropout_p
+        self.max_length = max_length
+
+        self.attn = nn.Linear(self.hidden_size * 2, self.max_length)
+        self.attn_combine = nn.Linear(self.hidden_size * 2, self.hidden_size)
+        self.dropout = nn.Dropout(self.dropout_p)
+        self.gru = nn.LSTM(input_size=100, hidden_size=100//2,
+                num_layers=2, batch_first=True,
+                dropout=0.3, bidirectional=True)
+        self.out = nn.Linear(self.hidden_size, self.output_size)
+
+    def forward(self, input, hidden, encoder_outputs):
+
+        attn_weights = F.softmax(
+            self.attn(torch.cat((input, hidden), 2)), dim=1)
+        attn_applied = torch.bmm(attn_weights.unsqueeze(0),
+                                 encoder_outputs.unsqueeze(0))
+
+        output = torch.cat((embedded[0], attn_applied[0]), 1)
+        output = self.attn_combine(output).unsqueeze(0)
+
+        output = F.relu(output)
+        output, hidden = self.gru(output, hidden)
+
+        output = F.log_softmax(self.out(output[0]), dim=1)
+        return output, hidden, attn_weights
+
+    def initHidden(self):
+        hidden = torch.zeros(1, 1, self.hidden_size)
+        if torch.cuda.is_available():
+            hidden = hidden.cuda()
+        return hidden
+
+
 class FromPredictor(nn.Module):
     def __init__(self, N_word, N_h, N_depth, gpu, use_hs, bert, onefrom, wikisql_style=False):
         super(FromPredictor, self).__init__()
